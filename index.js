0a33515474d92a771c0fa09025370fafcfe2b4d2
whiibobeautun-eng <263344301+whiibobeautun-eng@users.noreply.github.com>
2026-06-04 15:08:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -304,7 +304,8 @@ const commands = [
     .setName('echanger')
     .setDescription('Propose un échange de cartes avec un autre joueur')
     .addUserOption(o => o.setName('joueur').setDescription('Le joueur avec qui échanger').setRequired(true)),
-new SlashCommandBuilder()
+-];
+  new SlashCommandBuilder()
   .setName('setuptickets')
   .setDescription('[STAFF] Envoie le panneau de tickets dans ce salon'), ]
     });
